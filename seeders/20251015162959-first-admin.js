f78156d5e69cd438767704f479d8c881000a1d99
Muhammad Akhyar <akhyarakhyar725@gmail.com>
2025-10-15 23:42:02 +0700

    chore: add seed

---
+++ seeders/20251015162959-first-admin.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          nim: "0000",
+          name: "admin",
+          role: "admin",
+          password: "7e784ec44421a76be80302b66bca93e129ba3b17",
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
