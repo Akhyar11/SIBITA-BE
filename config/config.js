7a2c4c6c61c89c4623d677abbbae1a6542074b8e
Muhammad Akhyar <akhyarakhyar725@gmail.com>
2025-10-15 11:57:42 +0700

    chore: setup config sequilize

--- config/config.js
+++ config/config.js
@@ -10,4 +10,22 @@ module.exports = {
     dialect: "mysql",
     logging: false,
   },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false,
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false,
+  },
 };
